f6e4b04c4c3ececa52e0a08e13d23fa18df3bfca
floatGray <599316783@qq.com>
2024-04-22 10:00:45 +0800

    feat: 文件上传逻辑

--- app/page.js
+++ app/page.js
@@ -1,23 +1,26 @@
-import {Button} from "@/components/ui/button"
-import {Input} from "@/components/ui/input"
-import ReadFileContent from '@/components/readFile';
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import ReadFile from '@/components/ReadFile';
+import FileUploader from '@/components/FileUploader';
+import Link from 'next/link';
 function Home() {
-    const onFileChange = () => {
-        console.log(1)
-    }
-    //const router = useRouter()
-    const xyzFileContent = ReadFileContent('a.xyz');
-    const jsonFileContent = ReadFileContent('b.json');
-    return (
-        <>
-            {/*<Input type="file" id="上传文件"></Input>*/}
-            <Button><Link href={'/result'}>查看结果</Link></Button>
-            <p className="border-4 border-fuchsia-950">{xyzFileContent}</p>
-            <p>{jsonFileContent}</p>
-        </>
-
-    );
+  const onFileChange = () => {
+    console.log(1);
+  };
+  //const router = useRouter()
+  const xyzFileContent = ReadFile('a.xyz');
+  const jsonFileContent = ReadFile('b.json');
+  return (
+    <>
+      {/*<Input type="file" id="上传文件"></Input>*/}
+      <FileUploader />
+      <Button>
+        <Link href={'/result'}>查看结果</Link>
+      </Button>
+      <p className="border-4 border-fuchsia-950">{xyzFileContent}</p>
+      <p>{jsonFileContent}</p>
+    </>
+  );
 }
 
-export default Home
+export default Home;
