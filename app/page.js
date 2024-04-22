72be0f7c1708fb35ffa9c4a7b9a25fee8ad2581d
floatGray <599316783@qq.com>
2024-04-22 11:09:05 +0800

    fix: 引入动态xyz

--- app/page.js
+++ app/page.js
@@ -3,13 +3,15 @@ import { Input } from '@/components/ui/input';
 import ReadFile from '@/components/ReadFile';
 import FileUploader from '@/components/FileUploader';
 import Link from 'next/link';
-function Home() {
+async function Home() {
   const onFileChange = () => {
     console.log(1);
   };
+
   //const router = useRouter()
-  const xyzFileContent = ReadFile('a.xyz');
+  const xyzFileContent = await ReadFile('a.xyz');
   const jsonFileContent = ReadFile('b.json');
+
   return (
     <>
       {/*<Input type="file" id="上传文件"></Input>*/}
--- components/readFile.js
+++ components/readFile.js
@@ -4,7 +4,6 @@ import path from 'path';
 const ReadFile = async (fileName) => {
   const filePath = path.join(process.cwd(), `/public/assets/${fileName}`);
   const fileContent = fs.readFileSync(filePath, 'utf8');
-
   return fileContent;
 };
 
